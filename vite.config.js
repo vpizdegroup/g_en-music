266842b8ad1d6d2800fb07c2106c01d833577883
vpizdegroup <yemlapshu@gmail.com>
2026-06-06 17:12:13 +0300

    Allow all hosts in Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    middlewareMode: false
+    allowedHosts: 'all'
   },
   preview: {
     port: 3000,
     host: '0.0.0.0',
-    middlewareMode: false
+    allowedHosts: 'all'
   }
 })
